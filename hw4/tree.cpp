6983df0fc6ae4ae0ef71c69ef5a70fe8ec52226e
Ericlee0210 <imdfmy@gmail.com>
2015-05-07 21:32:43 +0800

    tree.cpp can read file, accepting 2 arguments, filename & epsilon

--- hw4/tree.cpp
+++ hw4/tree.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
+#include <cstdlib>   //atof()
+#include <iostream> //printf()
+#include <fstream>  //fstream fopen()
 using namespace std;
 
 
 
-int main()
+int main(int argc, char *argv[])
 {
-
+	FILE * pfile;
+	double epsilon;
+	char * epsilon_c;
+	char * fileName = NULL;
+	if( argc == 3 )
+	{
+		fileName = argv[1];
+		epsilon_c = argv[2];
+		epsilon = atof(epsilon_c);
+	}
+	else
+	{
+		printf("arrguments are wrong!!\n");
+		return 0;
+	}
+	pfile = fopen(fileName, "r");
+	if(!pfile)
+	{
+		printf("can't open %s .\n", fileName);
+		return 0;
+	}
+	else
+	{
+		//讀檔
+	}
+	fclose(pfile);
+	return 0;
 }
