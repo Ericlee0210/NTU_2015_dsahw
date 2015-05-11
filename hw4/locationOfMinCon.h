6bd843227b47b7d67981193b304e7f6371e60b62
Ericlee0210 <imdfmy@gmail.com>
2015-05-11 19:53:10 +0800

    create a constructor for locatoinOfMinCon class

--- hw4/locationOfMinCon.h
+++ hw4/locationOfMinCon.h
@@ -7,6 +7,7 @@ struct locationOfMinCon
 	int index_for_features;
 	double total_confusion;
 	double threshold;
+	locationOfMinCon(int a=0, int b=0, int c=100, int d=0): index_for_example(a), index_for_features(b), total_confusion(c), threshold(d){}
 };
 
 #endif
