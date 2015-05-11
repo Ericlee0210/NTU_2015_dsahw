6c352097c602ba89689361e8f4b7a8580f657765
Ericlee0210 <imdfmy@gmail.com>
2015-05-11 14:03:51 +0800

    overload swap function, and let array memeber in example class turned into vector member

--- hw4/exampleClass.cpp
+++
@@ -1,9 +0,0 @@
-#include "exampleClass.h"
-
-void example::exampleInit()
-{
-	for(int i=0; i< maxfeatures; i++)
-	{
-		features[i] = 0;
-	}
-}
--- hw4/exampleClass.h
+++ hw4/exampleClass.h
@@ -1,3 +1,5 @@
+#include <vector>
+#include <algorithm>
 #ifndef EXAMPLECLASS_H
 #define EXAMPLECLASS_H
 #define maxfeatures 1025
@@ -6,9 +8,15 @@ class example
 {
 public:
 	int result;
-	double features[maxfeatures];
-	double confusion[maxfeatures];
-	void exampleInit();
+	std::vector<double> features;
+	std::vector<double> confusion;
+	example(int max=1025, int c=0, int d=100): features(max, c), confusion(max, d){}
+	friend void swap(example& a, example& b)
+	{
+		std::swap(a.result, b.result);
+		a.features.swap(b.features);
+		a.confusion.swap(b.confusion);
+	}
 };
 
 
